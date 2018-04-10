3018ccf42209369262703ae75c5d84fdd8e21307
Ethan Ray Mosqueda <ethanray19@gmail.com>
2018-04-10 13:14:10 +0800

    made blog

---
+++ CCDjango/posts/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import re_path
+
+urlpatterns = [
+	re_path(r'^$', views.index, name='index'),
+	re_path(r'details/(?P<id>\d+)/$', views.details, name='details'),
+]
