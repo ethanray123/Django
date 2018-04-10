cd2088e8e11becc3a4f1695ae0e75ea3430d6970
Ethan Ray Mosqueda <ethanray19@gmail.com>
2018-04-10 18:25:01 +0800

    fixed urls

--- CCDjango/posts/urls.py
+++ CCDjango/posts/urls.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 
 urlpatterns = [
 	re_path(r'^$', views.index, name='index'),
-	re_path(r'details/(?P<id>\d+)/$', views.details, name='details'),
+	re_path(r'^details/(?P<id>\d+)/$', views.details, name='details'),
 ]
